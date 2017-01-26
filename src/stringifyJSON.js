2222710a4f53aece29bf0571a471db19f03923be
Apurva Shastry <apurva.shastry@gmail.com>
2017-01-26 15:26:00 -0800

    stringifyJSON is now working.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+
+  var resultString = '';	
   // your code goes here
+  if (obj == null) {
+  	//null cases
+    resultString  = 'null';
+  } else if (obj === undefined) {
+    // undefined cases
+    resultString = undefined;
+  } else if (typeof obj === 'string') {
+  	// string cases
+  	resultString = '\"'+obj+'\"';
+  } else if (typeof obj === 'object') {
+  	// objects and arrays
+    if (Array.isArray(obj)) {
+    	//array cases
+      resultString += '[';
+      for (var i = 0; i < obj.length; i++) {
+        if (resultString[resultString.length - 1] !== '[') {
+            resultString += ',';
+    	}
+        resultString += stringifyJSON(obj[i]);
+      }
+      resultString += ']';
+    } else {
+      // object cases
+      resultString += '{';
+      for (var i in obj) {
+    	if (obj[i] !== undefined && (typeof obj[i]) !== 'function' ) {
+    	  console.log(obj[i]);	
+    	  console.log(typeof obj[i]);
+          if (resultString[resultString.length - 1] !== '{') {
+            resultString += ',';
+    	  }
+    	  resultString += stringifyJSON(i);
+          resultString += ':'
+          resultString += stringifyJSON(obj[i]);
+    	}
+      }
+      resultString += '}';
+    }
+  } else {
+  	resultString = obj.toString();
+  }
+
+  return resultString;
 };
